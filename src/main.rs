259fc9ab868952ce19f654eff010d0658778df37
Timon Schelling <timon.schelling@outlook.de>
2023-06-16 02:52:43 +0200

    -

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,24 @@
-use std::net::{IpAddr, SocketAddr};
-
-use clap::{arg, command, crate_authors, crate_description, crate_name, crate_version};
+use std::str::FromStr;
 
 use axum::{
     body::BoxBody,
     extract::Path,
-    http::{HeaderValue, Response, StatusCode},
+    http::{HeaderMap, HeaderValue, Response, StatusCode},
     response::{IntoResponse, Redirect},
     routing::get,
     Router,
 };
+use fast_uaparser::OperatingSystem;
 use rand::seq::SliceRandom;
-use reqwest::Client;
+use reqwest::{header::USER_AGENT, Client};
 use serde::{Deserialize, Serialize};
 
-#[tokio::main]
-async fn main() {
-    let matches = command!(crate_name!())
-        .version(crate_version!())
-        .author(crate_authors!())
-        .about(crate_description!())
-        .arg(arg!(-p --port <PORT> "Sets server port").default_value("8080"))
-        .arg(arg!(--host <HOST> "Sets server host").default_value("::1"))
-        .get_matches();
-
-    let host = match matches.get_one::<String>("host") {
-        Some(host) => host,
-        None => todo!(),
-    };
-
-    let ip = match host.parse::<IpAddr>() {
-        Ok(ip) => ip,
-        Err(_) => todo!(),
-    };
-
-    let port_str = match matches.get_one::<String>("port") {
-        Some(host) => host,
-        None => todo!(),
-    };
-
-    let port = match port_str.parse::<u16>() {
-        Ok(port) => port,
-        Err(_) => todo!(),
-    };
-
-    let address = SocketAddr::new(ip, port);
+#[shuttle_runtime::main]
+async fn routing() -> shuttle_axum::ShuttleAxum {
+    fast_uaparser::init();
 
     let router = Router::new().route("/:zone/:route", get(handler));
-
-    println!("Server listening on {}", address);
-    axum::Server::bind(&address)
-        .serve(router.into_make_service())
-        .await
-        .unwrap();
+    Ok(router.into())
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
@@ -64,13 +30,73 @@ struct Zone {
 #[derive(Serialize, Deserialize, Clone, Debug)]
 struct Route {
     name: Option<String>,
-    targets: Option<Vec<String>>,
+    target: Option<Target>,
 }
 
-async fn handler(Path((zone_name, route_name)): Path<(String, String)>) -> Response<BoxBody> {
-    let resp = get_raw_file_content("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".to_string()).await;
+#[derive(Serialize, Deserialize, Clone, Debug)]
+#[serde(tag = "type")]
+enum Target {
+    Url { url: String },
+    Random { targets: Vec<Target> },
+    YouTube { video: String },
+    Spotify { track: String },
+}
 
-    let resp = match resp {
+enum Os {
+    Linux,
+    Android,
+    Windows,
+    Darwin,
+    AppleMobile,
+    Unknown,
+}
+
+impl FromStr for Os {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "Linux" => Ok(Os::Linux),
+            "Android" => Ok(Os::Android),
+            "Windows" => Ok(Os::Windows),
+            "Darwin" | "Mac OS" | "Mac OS X" => Ok(Os::Darwin),
+            "iOS" | "tvOS" | "WatchOS" => Ok(Os::AppleMobile),
+            _ => Ok(Os::Unknown),
+        }
+    }
+}
+
+impl Target {
+    fn resolve(&self, headers: HeaderMap) -> Option<String> {
+        let user_agent_str = headers.get("user-agent")?.to_str().ok()?;
+        let os = user_agent_str.parse::<Os>().ok()?;
+
+        match self {
+            Target::Url { url } => Some(url.clone()),
+            Target::Random { targets } => {
+                let target = targets.choose(&mut rand::thread_rng())?;
+                target.resolve(headers)
+            },
+            Target::YouTube { video } => match os {
+                Os::Android => Some(format!("intent://youtu.be/{}#Intent;package=com.google.android.youtube;scheme=https;end", video)),
+                Os::AppleMobile => Some(format!("vnd.youtube://www.youtube.com/watch?v={}", video)),
+                _ => Some(format!("https://www.youtube.com/watch?v={}", video)),
+            },
+            Target::Spotify { track } => match os {
+                Os::Android | Os::AppleMobile => Some(format!("spotify://track/{}", track)),
+                _ => Some(format!("https://open.spotify.com/track/{}", track)),
+            },
+        }
+    }
+}
+
+async fn handler(
+    headers: HeaderMap,
+    Path((zone_name, route_name)): Path<(String, String)>,
+) -> Response<BoxBody> {
+    let resp = get_raw_file_content().await;
+
+    let zones_str = match resp {
         Ok(resp) => resp,
         Err(e) => {
             println!("Error: {:?}", e);
@@ -78,12 +104,6 @@ async fn handler(Path((zone_name, route_name)): Path<(String, String)>) -> Respo
         }
     };
 
-    let zones_str = resp;
-
-    // let zones_str = match std::fs::read_to_string("zones.yaml") {
-    //     Ok(s) => s,
-    //     Err(_) => return StatusCode::INTERNAL_SERVER_ERROR.into_response(),
-    // };
     let zones_str = zones_str.as_str();
 
     let zones = serde_yaml::Deserializer::from_str(zones_str).map(Zone::deserialize);
@@ -91,7 +111,10 @@ async fn handler(Path((zone_name, route_name)): Path<(String, String)>) -> Respo
     let zone = zones
         .filter_map(|r| match r {
             Ok(r) => Some(r),
-            Err(_) => None,
+            Err(e) => {
+                println!("Error {}", e);
+                None
+            }
         })
         .filter(|r| match r.name {
             Some(ref name) => name == &zone_name,
@@ -122,12 +145,12 @@ async fn handler(Path((zone_name, route_name)): Path<(String, String)>) -> Respo
         None => return StatusCode::NOT_FOUND.into_response(),
     };
 
-    let targets = match &route.targets {
-        Some(targets) => targets,
+    let target = match &route.target {
+        Some(target) => target,
         None => return StatusCode::NOT_FOUND.into_response(),
     };
 
-    let target = match targets.choose(&mut rand::thread_rng()) {
+    let target = match target.resolve(headers) {
         Some(target) => target,
         None => return StatusCode::NOT_FOUND.into_response(),
     };
@@ -137,12 +160,14 @@ async fn handler(Path((zone_name, route_name)): Path<(String, String)>) -> Respo
     Redirect::temporary(&target).into_response()
 }
 
-async fn get_raw_file_content(token: String) -> Result<String, reqwest::Error> {
+async fn get_raw_file_content() -> Result<String, reqwest::Error> {
     let client = Client::new();
 
     let owner = "timon-schelling";
     let repo = "rtrs";
     let path = "zones.yaml";
+
+    let token = "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";
 
     let url = format!(
         "https://api.github.com/repos/{}/{}/contents/{}",
