d6e1c09dd1194ffc89f4fe90143631e846fcfbdd
Timon Schelling <timon.schelling@outlook.de>
2023-06-16 12:37:09 +0200

    -

--- src/main.rs
+++ src/main.rs
@@ -90,7 +90,7 @@ impl Target {
             },
             Target::Image { url } => {
                 let text = format!("<img src=\"{}\"/>", url);
-                let encoded = base64::Engine::encode(&base64::engine::general_purpose::URL_SAFE, text.as_bytes());
+                let encoded = base64::Engine::encode(&base64::engine::general_purpose::STANDARD, text.as_bytes());
                 Some(format!("data:text/html;charset=utf-8;base64,{}", encoded))
             },
         }
